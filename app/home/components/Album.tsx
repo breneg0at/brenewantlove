34d259b3c6569acfe1b2112ace619ce41fd5c962
Brene <brendoleitepereira@gmail.com>
2024-10-02 20:36:51 -0300

    Feat: album main page

--- app/home/components/Album.tsx
+++ app/home/components/Album.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import mockData from "../mock/mockData"; // Certifique-se de que este caminho está correto
+import mockData from "../mock/mockData"; 
 import Image from "next/image";
 import divider from "@/assets/Linea.png";
 
@@ -7,24 +7,33 @@ const Album = () => {
   return (
     <div className="flex flex-col w-full h-fit">
       {mockData.map((item, index) => (
-        <div key={index} className="grid grid-cols-6 items-center w-full mb-4">
+        <div
+          key={index}
+          className="grid grid-cols-6 items-center w-full opacity-0 animate-fadeIn transition-all duration-700 ease-in-out"
+        >
           {index % 2 === 0 && (
             <div
               className={`flex flex-col justify-center items-center col-start-1 col-span-2 p-4`}
             >
-              <p className="textOne text-[var(--primary-color)] text-center">{item.date}</p>
-              <h2 className="textOne text-[var(--primary-color)] text-center">{item.title}</h2>
+              <p className="textOneVar text-[var(--primary-color)] text-center transition-transform duration-500 hover:scale-105">
+                {item.date}
+              </p>
+              <h2 className="textOneVar text-[var(--primary-color)] text-center transition-transform duration-500 hover:scale-105">
+                {item.title}
+              </h2>
             </div>
           )}
-          <div className="col-start-3 col-span-2 flex items-center justify-center">
-            <div className="flex items-center justify-center bg-white border-white shadow-lg rounded-lg w-[16rem] h-[11rem] p-2">
+
+          <div className="col-start-3 col-span-2 flex items-center justify-center relative">
+            <div className="flex items-center justify-center bg-white border-white shadow-lg rounded-lg w-[16rem] h-[11rem] p-2 relative overflow-hidden transition-transform duration-500 hover:scale-105 hover:shadow-2xl">
               <Image
                 src={item.imageUrl}
                 alt={item.title}
                 width={196}
                 height={196}
-                className="rounded h-[9rem] w-[16rem] mb-3"
+                className="rounded h-[9rem] w-[16rem] mb-3 transition-opacity duration-500 hover:opacity-90"
               />
+              <div className="absolute inset-0 bg-black bg-opacity-30 rounded-lg transition-opacity duration-500 hover:bg-opacity-50"></div>
             </div>
           </div>
 
@@ -32,20 +41,23 @@ const Album = () => {
             <div
               className={`flex flex-col justify-center items-center col-start-5 col-span-2 p-4`}
             >
- <p className="textOne text-[var(--primary-color)] text-center">{item.date}</p>
-              <h2 className="textOne text-[var(--primary-color)] text-center">{item.title}</h2>
+              <p className="textOneVar text-[var(--primary-color)] text-center transition-transform duration-500 hover:scale-105">
+                {item.date}
+              </p>
+              <h2 className="textOneVar text-[var(--primary-color)] text-center transition-transform duration-500 hover:scale-105">
+                {item.title}
+              </h2>
             </div>
           )}
 
-          {/* Divisor entre os itens, se não for o último */}
           {index < mockData.length - 1 && (
-            <div className="flex items-center justify-center col-span-6 mt-2">
+            <div className="flex items-center justify-center col-span-6">
               <Image
                 src={divider}
                 alt="Divider"
                 width={15}
                 height={18}
-                className="object-contain"
+                className="object-contain transition-opacity duration-500 hover:opacity-70"
               />
             </div>
           )}
