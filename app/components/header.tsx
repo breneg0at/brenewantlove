9d8fa0e5e80d8f8526f05c0bab65506eb324b509
Brene <brendoleitepereira@gmail.com>
2024-10-02 21:32:32 -0300

    Fix: improve logo

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import Polalov3 from "./polalov3";
+import Polalov3 from "@/assets/logo.png";
+import Image from "next/image";
 
 const Header = () => {
   return (
     <header className="flex items-center justify-center h-[80px] bg-transparent relative z-50">
       <div className="logo-container relative hover:scale-105 transition-transform duration-300 ease-in-out">
-        <Polalov3 />
+        <Image src={Polalov3} alt="" width={112} height={49} />
       </div>
     </header>
   );
