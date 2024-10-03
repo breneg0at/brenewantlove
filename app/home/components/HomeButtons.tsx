eb4196c2879eacdb63a5837abf49150a05faef31
Brene <brendoleitepereira@gmail.com>
2024-10-03 11:03:17 -0300

    Fix: adjust button padding

--- app/home/components/HomeButtons.tsx
+++ app/home/components/HomeButtons.tsx
@@ -7,22 +7,22 @@ const HomeButtons = () => {
   return (
     <div className="flex flex-row w-full fixed bottom-8 container justify-between pr-8 pl-8">
       <>
-        <button className="focus:outline-none bg-[var(--bg-color)] rounded-full p-7">
+        <button className="focus:outline-none rounded-full p-5">
           <Image
             src={leftButtonImage}
             alt="Left Button"
-            width={52}
-            height={52}
+            width={36}
+            height={36}
             className="transition-transform duration-300 hover:scale-105"
           />
         </button>
       </>
-      <button className="focus:outline-none bg-[var(--bg-color)] rounded-full p-7">
+      <button className="focus:outline-none rounded-full p-5">
         <Image
           src={rightButtonImage}
           alt="Right Button"
-          width={52}
-          height={52}
+          width={36}
+          height={36}
           className="transition-transform duration-300 hover:scale-105"
         />
       </button>
